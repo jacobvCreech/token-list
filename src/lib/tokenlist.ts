2b070cd69073b5ac918494ffb665da11e0c014cc
jacobvCreech <gaming.bradda01@gmail.com>
2024-06-08 12:33:17 +0930

    Minor documentation update

--- src/lib/tokenlist.ts
+++ src/lib/tokenlist.ts
@@ -631,3 +631,4 @@ export class TokenListContainer {
 // Update on 2024-06-07 08:13:44: Refactored function - 5825
 // Update on 2024-06-08 09:23:20: Refactored some code - 2494
 // Update on 2024-06-08 12:47:43: Updated README - 9972
+// Update on 2024-06-08 12:33:17: Minor documentation update - 5736
