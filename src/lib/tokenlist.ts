019ffa55282bec0ed4f33854eee85f3430bb3ab7
jacobvCreech <gaming.bradda01@gmail.com>
2024-09-25 19:23:17 +0930

    Added new feature

--- src/lib/tokenlist.ts
+++ src/lib/tokenlist.ts
@@ -792,3 +792,4 @@ export class TokenListContainer {
 // Update on 2024-09-24 11:27:28: Improved performance - 9973
 // Update on 2024-09-24 21:37:58: Minor documentation update - 8532
 // Update on 2024-09-25 18:13:15: Improved performance - 2518
+// Update on 2024-09-25 19:23:17: Added new feature - 9552
