674afb249d0cf60b964c5ad2a1ccab4b3bb5a6d4
jacobvCreech <gaming.bradda01@gmail.com>
2024-10-25 20:01:19 +1030

    Refactored function

--- src/lib/tokenlist.ts
+++ src/lib/tokenlist.ts
@@ -204,3 +204,4 @@ export class TokenListContainer {
 // Update on 2024-10-24 15:39:47: Refactored some code - 9672
 // Update on 2024-10-24 12:51:48: Small UI tweak - 3197
 // Update on 2024-10-24 19:17:53: Updated dependencies - 6206
+// Update on 2024-10-25 20:01:19: Refactored function - 4667
