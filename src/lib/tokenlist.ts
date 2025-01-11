0cff03cfc68d6c83b9224adb955a83244d0843f7
jacobvCreech <gaming.bradda01@gmail.com>
2025-01-11 13:23:39 +1030

    Code cleanup

--- src/lib/tokenlist.ts
+++ src/lib/tokenlist.ts
@@ -361,3 +361,4 @@ export class TokenListContainer {
 // Update on 2025-01-10 08:08:14: Refactored some code - 3328
 // Update on 2025-01-10 16:59:41: Updated README - 7059
 // Update on 2025-01-10 09:34:19: Minor documentation update - 9110
+// Update on 2025-01-11 13:23:39: Code cleanup - 5856
