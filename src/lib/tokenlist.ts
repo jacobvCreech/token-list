e1b1bad9cc1c461782ce7272f5743c239baec775
jacobvCreech <gaming.bradda01@gmail.com>
2025-02-17 20:08:03 +1030

    Refactored some code

--- src/lib/tokenlist.ts
+++ src/lib/tokenlist.ts
@@ -434,3 +434,4 @@ export class TokenListContainer {
 // Update on 2025-02-15 20:20:03: Updated dependencies - 1278
 // Update on 2025-02-16 13:25:24: Updated dependencies - 6756
 // Update on 2025-02-16 09:53:19: Updated README - 5745
+// Update on 2025-02-17 20:08:03: Refactored some code - 6085
