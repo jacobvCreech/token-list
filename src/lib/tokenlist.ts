77e523ce9a7f686f48e5c9bcb439d8e77a5bf827
jacobvCreech <gaming.bradda01@gmail.com>
2025-04-08 19:08:27 +0930

    Updated dependencies

--- src/lib/tokenlist.ts
+++ src/lib/tokenlist.ts
@@ -537,3 +537,4 @@ export class TokenListContainer {
 // Update on 2025-04-07 16:32:22: Improved performance - 4114
 // Update on 2025-04-07 12:58:29: Code cleanup - 6345
 // Update on 2025-04-08 15:19:27: Minor documentation update - 6770
+// Update on 2025-04-08 19:08:27: Updated dependencies - 5706
